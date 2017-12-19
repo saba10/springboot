852bdbc3c83bb059896ad2f45cfea5123711693d
kuruva <suresh.kuruva@capgemini.com>
2017-12-19 17:17:21 +0530

    added

---
+++ SpringBootApp-Chaitanya/src/main/java/com/capgemini/SimpleRestController.java
@@ -0,0 +1,16 @@
+package com.capgemini;
+	
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleRestController {
+	@GetMapping("/example")
+	public String example() {
+		return "Hello User " + new Date();
+	}
+}
+
